011e7d299a4c445bc7c2eed813561a6ef62882c1
strulala <leopold@strobl-rieden.de>
2022-11-03 16:07:29 -0500

    added Demodulator Input settings (Downconverter and LNB Power), added ASI2 Routing (ext ASI Out)

--- index.js
+++ index.js
@@ -120,6 +120,30 @@ instance.prototype.init_actions = function(system) {
 		}
 	};
 
+	actionsArr.ASI2OutputMux = {
+		label: 'ASI Out Source',
+		options: [
+			{
+				type:    'dropdown',
+				label:   'Choose Source',
+				id:      'ASI2OutputMuxSource',
+				width:   12,
+				default: 'DB_P17101_ASI2_OUTPUT_MUX&value=Demodulator',
+				choices:	[
+					{ id: 'DB_P17101_ASI2_OUTPUT_MUX&value=Demodulator',		label: 'Demodulator' },
+					{ id: 'DB_P17101_ASI2_OUTPUT_MUX&value=Ext ASI',		label: 'Ext ASI' },
+					{ id: 'DB_P17101_ASI2_OUTPUT_MUX&value=Diversity',		label: 'Diversity' },
+					{ id: 'DB_P17101_ASI2_OUTPUT_MUX&value=Off',		label: 'Off' },
+				]
+			},
+		],
+		callback: function(action, bank) {
+			let cmd = action.options.ASI2OutputMuxSource;
+			self.log('debug', 'CMD Send: ' + cmd);
+			self.sendCommand(cmd);
+		}
+	};
+
 	actionsArr.ULRXFrequency = {
 
 		
@@ -252,6 +276,179 @@ instance.prototype.init_actions = function(system) {
 			self.sendCommand(cmd);
 		}
 	};
+
+	actionsArr.ULRXDownConverter = {
+		label: 'DownConverter',
+		options: [
+			{
+				type:    'dropdown',
+				label:   'Choose Downconverter',
+				id:      'ULRXDownConverter',
+				width:   12,
+				default: 'DB_ULRX_DOWN_CONVERTER&value=L3025-1927E',
+				choices:	[
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3010Lo',		label: 'L3010Lo' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3010Hi',		label: 'L3010Hi' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3014',		label: 'L3014' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3030',		label: 'L3030' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3033',		label: 'L3033' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3034',		label: 'L3034' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3037',		label: 'L3037' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3039',		label: 'L3039' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3060',		label: 'L3060' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3066',		label: 'L3066' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3080',		label: 'L3080' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3085',		label: 'L3085' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3090',		label: 'L3090' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L9234',		label: 'L9234' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-1415',		label: 'L3025-1415' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-1718',		label: 'L3025-1718' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-1927',		label: 'L3025-1927' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-1927E',		label: 'L3025-1927E' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-2024',		label: 'L3025-2024' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-2529',		label: 'L3025-2529' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-2732',		label: 'L3025-2732' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-3236',		label: 'L3025-3236' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-3239E',		label: 'L3025-3239E' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-4450',		label: 'L3025-4450' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-5459',		label: 'L3025-5459' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-6471',		label: 'L3025-6471' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-6875',		label: 'L3025-6875' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-7277',		label: 'L3025-7277' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=L3025-6368',		label: 'L3025-6368' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=Other',		label: 'Other' },
+					{ id: 'DB_ULRX_DOWN_CONVERTER&value=None',		label: 'None' },	
+					
+					
+				]
+			},
+		],
+		callback: function(action, bank) {
+			let cmd = action.options.ULRXDownConverter;
+			self.log('debug', 'CMD Send: ' + cmd);
+			self.sendCommand(cmd);
+		}
+	};
+
+	actionsArr.ULRXOtherLO = {
+
+		
+		label: 'Other LO Frequency',
+		options: [
+			{
+					type:    'textinput',
+					label:   'LO Frequency (0.0068-10.00GHz, example: 2.45)',
+					id:      'lofrequency'
+			}, 
+			{
+				type:    'dropdown',
+				label:   'Choose LO Polarity',
+				id:      'P17101_LO_MIX',
+				width:   12,
+				default: 'DB_P17101_LO_MIX&value=Low side',
+				choices:	[
+					{ id: 'DB_P17101_LO_MIX&value=Low side',		label: 'Low side' },
+					{ id: 'DB_P17101_LO_MIX&value=High side',		label: 'High side' },
+				
+					
+					
+				]
+			}
+		],
+	
+		callback: function(action, bank) {
+			let cmd1 = 'DB_ULRX_OTHER_LO&value=' + action.options.lofrequency;
+			self.log('debug', 'CMD Send: ' + cmd1);
+			self.sendCommand(cmd1);
+			let cmd2 = action.options.P17101_LO_MIX;
+			self.log('debug', 'CMD Send: ' + cmd2);
+			self.sendCommand(cmd2);
+		}
+		  
+	};
+
+	actionsArr.ULRXOtherLO = {
+
+		
+		label: 'LNB Power',
+		options: [
+			{
+				type: 'checkbox',
+				label: 'LNB1 Power',
+				id: 'lnb1power',
+				default: true
+			}, 
+			
+			{
+				type: 'checkbox',
+				label: 'LNB2 Power',
+				id: 'lnb2power',
+				default: true
+			}, 
+			{
+				type: 'checkbox',
+				label: 'LNB3 Power',
+				id: 'lnb3power',
+				default: true
+			}, 
+			{
+				type: 'checkbox',
+				label: 'LNB4 Power',
+				id: 'lnb4power',
+				default: true
+			}, 
+			
+		],
+	
+		callback: function(action, bank) {
+
+			if(action.options.lnb1power){
+				let cmd = 'DB_P17101_LNB_PWR1&value=On';
+				self.log('debug', 'CMD Send: ' + cmd);
+				self.sendCommand(cmd);
+			}else{
+				let cmd = 'DB_P17101_LNB_PWR1&value=Off';
+				self.log('debug', 'CMD Send: ' + cmd);
+				self.sendCommand(cmd);
+			}
+
+			if(action.options.lnb2power){
+				let cmd = 'DB_P17101_LNB_PWR2&value=On';
+				self.log('debug', 'CMD Send: ' + cmd);
+				self.sendCommand(cmd);
+			}else{
+				let cmd = 'DB_P17101_LNB_PWR2&value=Off';
+				self.log('debug', 'CMD Send: ' + cmd);
+				self.sendCommand(cmd);
+			}
+
+			if(action.options.lnb3power){
+				let cmd = 'DB_P17101_LNB_PWR3&value=On';
+				self.log('debug', 'CMD Send: ' + cmd);
+				self.sendCommand(cmd);
+			}else{
+				let cmd = 'DB_P17101_LNB_PWR3&value=Off';
+				self.log('debug', 'CMD Send: ' + cmd);
+				self.sendCommand(cmd);
+			}
+
+			if(action.options.lnb4power){
+				let cmd = 'DB_P17101_LNB_PWR4&value=On';
+				self.log('debug', 'CMD Send: ' + cmd);
+				self.sendCommand(cmd);
+			}else{
+				let cmd = 'DB_P17101_LNB_PWR4&value=Off';
+				self.log('debug', 'CMD Send: ' + cmd);
+				self.sendCommand(cmd);
+			}
+			
+			
+		}
+		  
+	};
+
+
+
 
 
 
